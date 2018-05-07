98e07452533307f0c55c1d6a3deb0c826b16d867
yj26141780712 <261417807@qq.com>
2018-05-07 13:09:34 +0800

    修改css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppComponent } from './app.component';
 import { FixTableComponent } from './fix-table/fix-table.component';
@@ -13,7 +14,9 @@ import { FixTableComponent } from './fix-table/fix-table.component';
     FixTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/fix-table/fix-table.component.ts
+++ src/app/fix-table/fix-table.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-fix-table',
@@ -7,17 +8,94 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FixTableComponent implements OnInit {
 
-  scrollTop:number;
-  scrollLeft:number;
-  constructor() { }
+  //表宽度
+  //固定列宽度 w_gird_fixed 
+  //非固定列宽度 w_gird_nofixed
+  //表高度
+  //表头高度 h_gird_head
+  //表身高度 h_gird_body
+  //表格实际宽度
+
+  config = {
+    h_gird: '262px',
+    h_gird_head: '45px',
+    h_gird_body: '200px',
+    w_gird: '802px',
+    w_gird_fixed: '200px',
+    w_gird_nofixed: '600px',
+    w_gird_table: '1000px',
+    columns: [
+      { field: 'm_id', title: '注塑机编号', fixed: true, width: 100 },
+      { field: 'm_name', title: '注塑机名称', fixed: true, width: 100 },
+      { field: 'm_type', title: '注塑机类型', fixed: true, width: 100 },
+      { field: 'c_id', title: '采集器编号', fixed: false, width: 100 },
+      { field: 'o_name', title: '出厂调试人员', fixed: false, width: 100 },
+      { field: 'o_date', title: '出厂日期', fixed: false, width: 100 },
+      { field: 'area', title: '所属片区', fixed: false, width: 100 },
+      { field: 'o_company', title: '出厂公司', fixed: false, width: 100 },
+      { field: 'd_company', title: '代理公司', fixed: false, width: 100 },
+      { field: 's_company', title: '塑料厂', fixed: false, width: 100 },
+    ]
+  }
+  source: any[];
+  scrollTop: number;
+  scrollLeft: number;
+  constructor(private http: Http) { }
 
   ngOnInit() {
+    let w_gird_fixed = 0; //固定列宽度
+    let w_gird_nofixed = 600; //非固定列默认宽度
+    let w_gird_table = 0;
+    let h_gird_head = 45;
+    let h_gird_body = 200;
+    for (let col of this.config.columns) {
+      if (col.fixed) {
+        w_gird_fixed += col.width;
+      } else {
+        w_gird_table += col.width;
+      }
+    }
+    this.config.w_gird_fixed = w_gird_fixed + 'px';
+    this.config.w_gird_table = w_gird_table + 'px';
+    this.config.w_gird = (w_gird_fixed + w_gird_nofixed + 2) + 'px';
+    this.config.h_gird_head = h_gird_head + 'px';
+    this.config.h_gird_body = h_gird_body + 'px';
+    this.config.h_gird = (h_gird_head + h_gird_body + 2) + 'px';
+
+    let _url = "http://192.168.2.229:8088/IMS/api/apideviceList.action";
+    this.http.get(_url).subscribe(res => {
+      let json = res.json();
+      if (json.code == 200) {
+        let data = json.obj;
+        var array = [];
+        for (var i = 0; i < data.length; i++) {
+          var item = { m_id: "", m_name: "", m_type: "", c_id: "", o_name: "", o_date: "", area: "", o_company: "", d_company: "", s_company: "", remarks: "", id: "", x: "", y: "" };
+          item.m_id = data[i].sn;
+          item.m_name = data[i].name;
+          item.m_type = data[i].modelName;
+          item.c_id = data[i].monitorid;
+          item.o_name = data[i].cpersonnel || '';
+          if (data[i].ddate != null) {
+            item.o_date = data[i].ddate.substring(0, 10);
+          }
+          item.area = data[i].areaName;
+          item.o_company = data[i].companyName;
+          item.d_company = data[i].proxyName;
+          item.s_company = data[i].factoryName;
+          item.id = data[i].id;
+          item.x = data[i].x;
+          item.y = data[i].y;
+          array.push(item);
+        }
+        this.source = [].concat(array);
+      }
+    });
   }
 
-  scroll(event){
-    console.log(event,event.target.scrollTop);
-    this.scrollTop=event.target.scrollTop;
-    this.scrollLeft=event.target.scrollLeft;
+  scroll(event) {
+    console.log(event, event.target.scrollTop);
+    this.scrollTop = event.target.scrollTop;
+    this.scrollLeft = event.target.scrollLeft;
     console.log(this.scrollLeft);
     //let _top =document.getElementById('top');
     //_top.scrollTop=this.scrollTop;
@@ -30,7 +108,7 @@ export class FixTableComponent implements OnInit {
     //document.getElementById("head")._scrollContent.nativeElement.scrollLeft = document.getElementById("right")._scrollContent.nativeElement.scrollLeft;
     //document.getElementById("left")._scrollContent.nativeElement.scrollTop = document.getElementById("right")._scrollContent.nativeElement.scrollTop;
   }
-  test(){
+  test() {
     console.log(123);
   }
 }
